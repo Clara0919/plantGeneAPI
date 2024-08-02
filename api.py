fd65e7701cc0b5e77207a628a030affcdec973bb
Clara0919 <mansyuan.he@gmail.com>
2024-08-02 08:37:45 +0800

    feat:  新增取得 locus id api

--- api.py
+++ api.py
@@ -2,13 +2,18 @@ from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request
 import getData
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
 
 
 
 app = Flask(__name__)
 @app.route('/api/getData', methods=['GET'])
 def get_data():
-    data, status_code = getData.getGeneData()
+    # 用於傳入基因位置 request.args.get('position') 
+    position= 'Chr1:1,489,366..1,499,365'
+    # 暫時用於傳入sessionId request.args.get('position') 
+    sessionId='302EEB1F6096906C540B6C36553A1134'
+    data, status_code = getData.getGeneData(position,sessionId)
     
     if status_code==200:
         context=data
@@ -20,7 +25,11 @@ def get_data():
         
         for area in areas:
             href = area.get('href')
-            area_data.append({'href': href.replace('\\\"', '')})
+            cleanHref=href.replace('\\\"', '')
+            parsed_url = urlparse(cleanHref)
+            query_params = parse_qs(parsed_url.query)
+            name = query_params.get("name", [None])[0]
+            area_data.append(name)
         return jsonify({"status": "成功", "data": area_data}), 200
 
             
@@ -28,8 +37,15 @@ def get_data():
     else:   
         return jsonify({"status": "失敗", "data": data}), status_code
 
-  
-            
+@app.route('/api/getId', methods=['GET'])
+def get_id():
+    position = request.args.get('position')
+    sessionId=request.args.get('sessionId')
+    data, status_code = getData.getLocusId(position,sessionId)
+    if status_code==200:
+        return data
+    else:
+        return '取得 locus id 失敗'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- getData.py
+++ getData.py
@@ -1,8 +1,27 @@
 import requests
+import json
+import urllib.parse
 
-def getGeneData():
+headers = {
+    'Accept-Language': 'zh-TW,zh;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Dnt': '1',
+    'Origin': 'https://gbrowse.arabidopsis.org',
+    'Pragma': 'no-cache',
+    'Referer': 'https://gbrowse.arabidopsis.org/cgi-bin/gb2/gbrowse/arabidopsis/',
+    'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"macOS"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+}
+
+
+
+def getSessionId():
     # 取得jsessionid
-    url = "https://gbrowse.arabidopsis.org/cgi-bin/gb2/gbrowse/arabidopsis/"
     session = requests.Session()
     getCookieHeaders = {
     'authority': 'www.googletagmanager.com',
@@ -26,53 +45,71 @@ def getGeneData():
     }
     cookieResponse = session.get('https://www.arabidopsis.org/',headers=getCookieHeaders)
     jsessionid = cookieResponse.cookies.get("JSESSIONID")
-    
-    
-    # 取得locus資料
-    headers = {
-    'Accept-Language': 'zh-TW,zh;q=0.9',
-    'Cache-Control': 'no-cache',
-    'Dnt': '1',
-    'Origin': 'https://gbrowse.arabidopsis.org',
-    'Pragma': 'no-cache',
-    'Referer': 'https://gbrowse.arabidopsis.org/',
-    'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"macOS"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Site': 'cross-site',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-}
     # print(jsessionid)
-    cookies = {
-     'JSESSIONID': 'B882D91F72680B769F9636EF658AFC3F',
+    return jsessionid
+
+def getLocusId(position,sessionId):
+    # 取得locus資料
+    files = {
+    'action': (None, 'navigate'),
+    'navigate': (None, 'left 0'),
+    'view_start': (None, 'NaN'),
+    'view_stop': (None, 'NaN'),
+    'snapshot': (None, 'false'),
 }
+    params = {
+    'name': position,
+}
+    cookies = {
+    'JSESSIONID': sessionId,
+}
+    
+    # 使用 requests.post 發送 POST 請求
+    response = requests.post(
+    'https://gbrowse.arabidopsis.org/cgi-bin/gb2/gbrowse/arabidopsis/',
+    cookies=cookies,
+    params=params,
+    headers=headers,
+    files=files
+)  
+    if response.status_code==200:
+    # 解析 JSON 資料
+        parsed_data = json.loads(response.text)
+    # 提取 locus 的值
+        locus_id = parsed_data["track_keys"]["Locus"]
+        return locus_id
+    else:
+        return ("無法取得locusId")
+
+def getGeneData(position,sessionId):
+    
+    url = "https://gbrowse.arabidopsis.org/cgi-bin/gb2/gbrowse/arabidopsis/"
+    
+    locusId=getLocusId(position,sessionId)
+
     files = {
     'action': (None, 'retrieve_multiple'),
     'track_ids': (None, 'Locus'),
-    'tk_BAC': (None, '02f81043270e01e70271c994cf26b37f'),
-    'tk_Locus': (None, 'f5743086a42323467d59420b38d34f39'),
-    'tk_ProteinCoding': (None, '499ac2f3b4a1c87929868c85f0d6974f'),
-    'tk_Pseudogene': (None, 'b4bfbb230c370b0bb74d86956a1b6b38'),
-    'tk_ncRNAs': (None, 'c01d5d87e8a42685a84df8498fd94611'),
-    'tk_cDNA': (None, '8a408d8c6c2af085ea18f605bf30458b'),
-    'tk_tDNAs': (None, 'a2bbb8fd84ac63670ded3f13cab62ea2'),
-    'tk_Polymorphism': (None, '556af7dcdb0c44bb3af02512fa70ef0c'),
-    'tk_Marker': (None, '42442feee001037d4422334e08055acf'),
+    'tk_Locus': (None, locusId),
+}
+    
+    cookies = {
+    'JSESSIONID': sessionId,
 }
 
     # 使用 requests.post 發送 POST 請求
     response = requests.post(
-    'https://gbrowse.arabidopsis.org/cgi-bin/gb2/gbrowse/arabidopsis/',
+    url,
     cookies=cookies,
     headers=headers,
     files=files
 )
     if response.status_code == 200:
-        # print(response.text)
+        print(response.text)
         return response.text, response.status_code
     else:
         # print("請求失敗，狀態:", response.status_code)
         return ("請求失敗，狀態:"), response.status_code
+    
+
 
